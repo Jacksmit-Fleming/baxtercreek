77467d76be365647e8adf782d1e16d534f9d9737
Jackson <146374730+Jacksmit-Fleming@users.noreply.github.com>
2024-06-17 13:45:07 -0400

    Add files via upload

---
+++ README.md
@@ -0,0 +1,5 @@
+# GEOM 99 Group Project Website
+
+## Project - Data Collection in a 3D Environment  
+
+
---
+++ script.js
@@ -0,0 +1,14 @@
+//- Owner : Rahul Saravanabavan
+//- Date  : Nov 28 (template)
+//- From  : https://www.w3schools.com/howto/howto_js_topnav_responsive.asp
+function showSidebar(){
+    const sidebar = document.querySelector('.sidebar')
+    sidebar.style.display = 'flex'
+}
+function hideSidebar(){
+    const sidebar = document.querySelector('.sidebar')
+    sidebar.style.display = 'none'
+}
+// End of Rahul Saravanabavan
+
+
